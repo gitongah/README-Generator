8449d42685f533eb9d5c20145dffa32e83b1a8e9
gitongah <mbaabugitonga@gmail.com>
2024-04-17 19:47:45 -0700

    created the diferent function to generate and render the markdown file

--- Develop/index.js
+++ Develop/index.js
@@ -5,32 +5,32 @@ const genearateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: 'text',
+    type: 'input',
     message: 'What is your projects name??',
     name:'title'
   },
   {
-    type: 'text',
+    type: 'input',
     message: 'Please write the description of your project',
     name:'description'
   },
   {
-    type: 'text',
+    type: 'input',
     message: 'please give details of the installation instructions',
     name:'instalation'
   },
   {
-    type: 'text',
+    type: 'input',
     message: 'Give details of how the project will be used.',
     name:'usage'
   },
   {
-    type: 'text',
+    type: 'input',
     message: 'Give detail of how developers can contribute to your project.',
     name:'contributing'
   },
   {
-    type: 'text',
+    type: 'input',
     message: 'Give detail of how tests are done on your project.',
     name:'test'
   },
@@ -38,8 +38,19 @@ const questions = [
     type: 'list',
     message: 'Which license would you like to use for your project?',
     name:'license',
-    choices:['MIT','GNU GPLv3','GNU LGPLv3', 'GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense']
+    choices:['MIT','ISC','GNU', 'Eclipse', 'None']
   },
+  {
+    type: 'text',
+    message: 'Enter your Github username',
+    name:'userName'
+  },
+    {
+    type: 'text',
+    message: 'Please enter your email',
+    name:'email'
+  },
+
   
 
 ];
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'MIT'){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }else if(license === 'ISC'){
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  }else if(license ==='GNU'){
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+  }else if(license ==='Eclipse'){
+    return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]`
+  }else if(license === 'None'){
+    return `![Badge](https://img.shields.io/badge/License-${license}-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT'){
+    return `[MIT Lincense](https://opensource.org/licenses/MIT)`
+  }else if(license === 'ISC'){
+    return `[ISC Lincense](https://opensource.org/licenses/ISC)`
+  }else if(license ==='GNU'){
+    return `[GNU License](https://www.gnu.org/licenses/gpl-3.0)`
+  }else if(license ==='Eclipse'){
+    return `[Eclipse License](https://opensource.org/licenses/EPL-1.0)`
+  }else if(license === 'None'){
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== 'None'){
+    return `## License 📝
+    This application is covered by the ${license} license.`
+  }
+  return ``;
+}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,34 +48,41 @@ function generateMarkdown(data) {
 
   # ${data.title}
 
-  ${data.license}
+  ${renderLicenseBadge(data.license)}
 
-  ### Description
+
+  ### Description 🔎
 
   ${data.description}
-
-  ### Table of Contents
-    * Installation
-
-
-  ## Installation
+  
+  ### Table of Contents 📖
+  - [Installation](#installation-⚙️)
+  - [Usage](#usage-🎮)
+  - [Contributing](#contributing-🧑‍🤝‍🧑)
+  - [Test](#test-🧪)
+  - [Questions](#questions-🙋)
  
+    
+  ## Installation ⚙️
+    ${data.instalation}
 
-   ## Usage
+  ## Usage 🎮
     ${data.usage}
 
-    ${data.license}
+  ${renderLicenseSection(data.license)}
+    ${renderLicenseLink(data.license)}
 
 
-  ## Contributing
-  ${data.contributing}
+  ## Contributing 🧑‍🤝‍🧑
+    ${data.contributing}
 
-  ## Test
+  ## Test  🧪
+    ${data.test}
 
-  ${data.test}
+  ## Questions 🙋
+    If there are any questions, feel free to contact my email at: ${data.email}
 
-
-
+    You can also find me on GitHub at: [${data.userName}](https://www.github.com/${data.userName})
 
 `;
 }
